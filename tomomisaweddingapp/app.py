da8b131d1eeef42544e35c4de42d183e0370d47e
tomoima525 <tomoima525@gmail.com>
2017-10-01 01:03:36 -0700

    add chat bot

--- tomomisaweddingapp/app.py
+++ tomomisaweddingapp/app.py
@@ -10,6 +10,44 @@ from cloudinary.utils import cloudinary_url
 import cloudinary.api
 from flask_sqlalchemy import SQLAlchemy
 
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage, ImageMessage,
+    VideoMessage, AudioMessage, FileMessage
+)
+
+# get channel_secret and channel_access_token from your environment variable
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+if channel_secret is None:
+    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
+
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
+
+static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
+
+# function for create tmp dir for download content
+def make_static_tmp_dir():
+    try:
+        os.makedirs(static_tmp_path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(static_tmp_path):
+            pass
+        else:
+            raise
+
+
+## init
 app = Flask(__name__, instance_relative_config=True)
 
 if app.debug:
@@ -80,6 +118,70 @@ def close_db(error):
     """Closes the database again at the end of the request."""
     if hasattr(g, '_database'):
         g._database.close()
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+## Handler for LINE chat bot
+@handler.add(MessageEvent, message=TextMessage)
+def handle_text_message(event):
+    text = event.message.text
+
+    if text == '写真一覧':
+        ## Show image list
+        link = 'https://tomomisa-wedding-1015.herokuapp.com/list'
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text=link))
+
+# Image Message Type
+@handler.add(MessageEvent, message=(ImageMessage, VideoMessage, AudioMessage))
+def handle_content_message(event):
+    if isinstance(event.message, ImageMessage):
+        ext = 'jpg'
+    else:
+        sorry_text='画像以外は送れません、ごめんなさい!'
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text=sorry_text))
+        return
+
+    message_content = line_bot_api.get_message_content(event.message.id)
+    with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix=ext + '-', delete=False) as tf:
+        for chunk in message_content.iter_content():
+            tf.write(chunk)
+        #tempfile_path = tf.name
+        upload_result = upload(tf.name)
+        if "error" in upload_result:
+            error_text='送信が失敗しました、もう一度トライしてみて下さい!'
+            line_bot_api.reply_message(
+                event.reply_token, TextSendMessage(text=error_text))
+            return
+
+        url, options = cloudinary_url(upload_result['public_id'], format = "jpg", crop = "fill", width = 100, height = 150)
+        cur = db.cursor()
+        cur.execute('insert into images (public_id, url) values (%s,%s)' , (upload_result['public_id'], url))
+        db.commit()
+    # dist_path = tempfile_path + '.' + ext
+    # dist_name = os.path.basename(dist_path)
+    # os.rename(tempfile_path, dist_path)
+        line_bot_api.reply_message(
+            event.reply_token, [
+                TextSendMessage(text='送信されました!'),
+                TextSendMessage(text=url)
+                ])
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
