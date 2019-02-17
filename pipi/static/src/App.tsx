3e789728730062753939747c2276dbbb41e5750d
Tomoaki Imai <tomoima525@gmail.com>
2019-02-17 17:33:24 +0900

    Sample to run React

---
+++ pipi/static/src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+interface Props {
+  test: string;
+}
+
+export const App = () => (
+  <div>
+    <h1>Pipi</h1>
+    <div>debug</div>
+  </div>
+);
---
+++ pipi/static/src/index.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-multi-assign */
+/* eslint-disable no-undef */
+import 'infinite-scroll';
+
+window.$ = window.jQuery = require('jquery');
